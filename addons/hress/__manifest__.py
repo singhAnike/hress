696c5f8fd7222b8ca5bb5b110383b169e9123aa8
Aniket <singh7509811123@gmail.com>
2025-06-20 14:52:48 +0530

    modifiyed manifest

---
+++ addons/hress/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Hress",
+    "summary": """ Employee self-service APIs for check-in/out, leave management, and payslip access.""",
+    "description": """
+        This module provides a set of RESTful APIs that allow employees to perform essential HR functions directly from external systems or mobile applications. Key features include:
+
+            - Check-In and Check-Out using API
+            - View current and past leave balances
+            - Submit leave requests
+            - Track leave status (approved, pending, rejected)
+            - Access and download salary slips
+
+            This module is ideal for building employee self-service portals or mobile apps integrated with Odoo HR.
+            """,
+    "author": "scidecs",
+    "website": "https://www.scidecs.com/en-US/",
+
+}
